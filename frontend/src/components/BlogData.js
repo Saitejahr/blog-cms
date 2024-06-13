5c476a7abb29da50973ecefed8faae4f3e12c60f
Saitejahr <saitejahr1100@gmail.com>
2024-06-13 14:24:21 +0530

    updates

--- frontend/src/components/BlogData.js
+++ frontend/src/components/BlogData.js
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import blog from './assests/blog.jpg'
-// import laptop from './assests/laptop.jpg'
-// import './styles/BlogData.css'
-
-// const BlogData = () => {
-//   return (
-//     <div className='data-container'>
-//       <img className='data-image' src={blog} alt={blog} />
-//       <h1 className='data-heading'>Welcome to MERN Stack</h1>
-//       <p className='data-para'>The MERN stack, comprising MongoDB, Express.js, React, and Node.js, is a cohesive set of technologies used for building efficient and scalable web applications. the MERN stack is a smart choice because it uses JavaScript across all layers, simplifying the learning curve. This uniformity, coupled with a strong community and ample learning resources, makes it an accessible and practical toolkit for anyone looking to dive into full-stack development.The MERN stack is also heavily utilized in the industry, favored by startups and large enterprises alike for its efficiency and the robust, modern web applications it can produce. This industry adoption not only validates its effectiveness but also opens up numerous career opportunities for those skilled in these technologies.</p>
-//       <img className='data-image' src={laptop} alt={laptop} />
-//       <h1 className='data-heading'>MERN Stack Blog </h1>
-//       <p className='data-para'>The MERN stack, comprising MongoDB, Express.js, React, and Node.js, is a cohesive set of technologies used for building efficient and scalable web applications. the MERN stack is a smart choice because it uses JavaScript across all layers, simplifying the learning curve. This uniformity, coupled with a strong community and ample learning resources, makes it an accessible and practical toolkit for anyone looking to dive into full-stack development.The MERN stack is also heavily utilized in the industry, favored by startups and large enterprises alike for its efficiency and the robust, modern web applications it can produce. This industry adoption not only validates its effectiveness but also opens up numerous career opportunities for those skilled in these technologies.</p>
-//     </div>
-//   )
-// }
-
-// export default BlogData
-
 import React from 'react'
 import blog from './assests/blog.jpg'
 import laptop from './assests/laptop.jpg'
@@ -26,31 +6,51 @@ import './styles/BlogData.css'
 const BlogData = () => {
   return (
     <div className='data-container'>
-      <div className='data-flex'>
-        <div><img className='data-image' src={blog} alt={blog} />
-      </div>
-      <div>
+      <img className='data-image' src={blog} alt={blog} />
       <h1 className='data-heading'>Welcome to MERN Stack</h1>
       <p className='data-para'>The MERN stack, comprising MongoDB, Express.js, React, and Node.js, is a cohesive set of technologies used for building efficient and scalable web applications. the MERN stack is a smart choice because it uses JavaScript across all layers, simplifying the learning curve. This uniformity, coupled with a strong community and ample learning resources, makes it an accessible and practical toolkit for anyone looking to dive into full-stack development.The MERN stack is also heavily utilized in the industry, favored by startups and large enterprises alike for its efficiency and the robust, modern web applications it can produce. This industry adoption not only validates its effectiveness but also opens up numerous career opportunities for those skilled in these technologies.</p>
-
-      </div>
-      
-      </div>
-      <div className='data-flex'>
-        <div>
-          <img className='data-image' src={laptop} alt={laptop} />
-        </div>
-        <div>
-        <h1 className='data-heading'>MERN Stack Blog </h1>
-        <p className='data-para'>The MERN stack, comprising MongoDB, Express.js, React, and Node.js, is a cohesive set of technologies used for building efficient and scalable web applications. the MERN stack is a smart choice because it uses JavaScript across all layers, simplifying the learning curve. This uniformity, coupled with a strong community and ample learning resources, makes it an accessible and practical toolkit for anyone looking to dive into full-stack development.The MERN stack is also heavily utilized in the industry, favored by startups and large enterprises alike for its efficiency and the robust, modern web applications it can produce. This industry adoption not only validates its effectiveness but also opens up numerous career opportunities for those skilled in these technologies.</p>
-        </div>
-      
-      
-
-      </div>
-      
+      <img className='data-image' src={laptop} alt={laptop} />
+      <h1 className='data-heading'>MERN Stack Blog </h1>
+      <p className='data-para'>The MERN stack, comprising MongoDB, Express.js, React, and Node.js, is a cohesive set of technologies used for building efficient and scalable web applications. the MERN stack is a smart choice because it uses JavaScript across all layers, simplifying the learning curve. This uniformity, coupled with a strong community and ample learning resources, makes it an accessible and practical toolkit for anyone looking to dive into full-stack development.The MERN stack is also heavily utilized in the industry, favored by startups and large enterprises alike for its efficiency and the robust, modern web applications it can produce. This industry adoption not only validates its effectiveness but also opens up numerous career opportunities for those skilled in these technologies.</p>
     </div>
   )
 }
 
 export default BlogData
+
+// import React from 'react'
+// import blog from './assests/blog.jpg'
+// import laptop from './assests/laptop.jpg'
+// import './styles/BlogData.css'
+
+// const BlogData = () => {
+//   return (
+//     <div className='data-container'>
+//       <div className='data-flex'>
+//         <div><img className='data-image' src={blog} alt={blog} />
+//       </div>
+//       <div>
+//       <h1 className='data-heading'>Welcome to MERN Stack</h1>
+//       <p className='data-para'>The MERN stack, comprising MongoDB, Express.js, React, and Node.js, is a cohesive set of technologies used for building efficient and scalable web applications. the MERN stack is a smart choice because it uses JavaScript across all layers, simplifying the learning curve. This uniformity, coupled with a strong community and ample learning resources, makes it an accessible and practical toolkit for anyone looking to dive into full-stack development.The MERN stack is also heavily utilized in the industry, favored by startups and large enterprises alike for its efficiency and the robust, modern web applications it can produce. This industry adoption not only validates its effectiveness but also opens up numerous career opportunities for those skilled in these technologies.</p>
+
+//       </div>
+      
+//       </div>
+//       <div className='data-flex'>
+//         <div>
+//           <img className='data-image' src={laptop} alt={laptop} />
+//         </div>
+//         <div>
+//         <h1 className='data-heading'>MERN Stack Blog </h1>
+//         <p className='data-para'>The MERN stack, comprising MongoDB, Express.js, React, and Node.js, is a cohesive set of technologies used for building efficient and scalable web applications. the MERN stack is a smart choice because it uses JavaScript across all layers, simplifying the learning curve. This uniformity, coupled with a strong community and ample learning resources, makes it an accessible and practical toolkit for anyone looking to dive into full-stack development.The MERN stack is also heavily utilized in the industry, favored by startups and large enterprises alike for its efficiency and the robust, modern web applications it can produce. This industry adoption not only validates its effectiveness but also opens up numerous career opportunities for those skilled in these technologies.</p>
+//         </div>
+      
+      
+
+//       </div>
+      
+//     </div>
+//   )
+// }
+
+// export default BlogData
--- frontend/src/components/ViewPost.js
+++ frontend/src/components/ViewPost.js
@@ -1,40 +1,29 @@
 import React from 'react'
 import './styles/ViewPost.css'
 import Data from './Data'
-import { useNavigate } from 'react-router-dom'
-
+import { NavLink } from 'react-router-dom'
 
 const ViewPost = () => {  
-    const navigate = useNavigate() 
-    const handleClick = () => {
-        navigate('/blogdata')
-    }
-  return (
-    <div className="blog-container">
-        <div className="blog-posts-grid">
-            {Data.map((data) => (
-                <div  key={data.id} className="blog-post">
+    
+  return ( 
+      <div className="blog-container">
+          <div className="blog-posts-grid">
+              {Data.map((data) => (
+                <NavLink to='/blogdata'>
+                  <div  key={data.id} className="blog-post">                 
                     <h2 className="blog-post-title">{data.title}</h2>
                     <p className="blog-post-content">{data.content}</p>
                     <img
                         className="blog-post-image"
                         src={data.img}
                         alt="images"
-                    />
-                    <button
-                  className="blog-button"
-                  onClick={handleClick}
-                >
-                  Know more
-                </button>
-                
-                </div>
-
-            ))
-                           
-            }
-        </div>
-    </div>
+                    />               
+                  </div>
+                </NavLink>
+              ))                          
+              }
+          </div>
+      </div>
     
   )
 }
